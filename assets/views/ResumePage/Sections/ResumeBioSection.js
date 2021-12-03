5fbca2a367985c30cc1772b642f95a59af6c30c1
Cory Siebler <cory.siebler@protonmail.com>
2021-12-03 12:18:42 -0700

    feat(Resume): revised bio about section

--- assets/views/ResumePage/Sections/ResumeBioSection.js
+++ assets/views/ResumePage/Sections/ResumeBioSection.js
@@ -1,14 +1,14 @@
-import React from "react";
-import { useTheme } from "@material-ui/core/styles";
-import useMediaQuery from "@material-ui/core/useMediaQuery";
-import Grid from "@material-ui/core/Grid";
-import Typography from "@material-ui/core/Typography";
-import Avatar from "@material-ui/core/Avatar";
-import avatar from "images/resume-avatar.jpg";
+import React from 'react';
+import { useTheme } from '@material-ui/core/styles';
+import useMediaQuery from '@material-ui/core/useMediaQuery';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+import Avatar from '@material-ui/core/Avatar';
+import avatar from 'images/resume-avatar.jpg';
 
 const ResumeBioSection = () => {
   const theme = useTheme();
-  const matches = useMediaQuery(theme.breakpoints.up("md"));
+  const matches = useMediaQuery(theme.breakpoints.up('md'));
 
   return (
     <Grid
@@ -24,10 +24,12 @@ const ResumeBioSection = () => {
       <Grid item md={8} xs={12}>
         <Typography variant="body1">
           Software engineer with a wide array of talents and expertise including
-          web development, database administration, mobile application
-          development, information technology, and project management. Looking
-          for a senior software engineering position where I am building new and
-          innovative applications.
+          web development, database administration, and project management.
+          Looking for a senior software engineering position where I am building
+          new and innovative applications. Prefer Python and PHP work for
+          back-end applications, but interested in Go as a new and upcoming
+          technology. Currently focused on React.js front-end software using
+          hooks for ease in state management and efficiency of rendering.
         </Typography>
       </Grid>
     </Grid>
