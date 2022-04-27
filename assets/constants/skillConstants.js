0f0a5aee4cba8781157ddacf739ebfae8275874c
Cory Siebler <cory.siebler@protonmail.com>
2022-04-27 13:28:20 -0700

    feat(skillConstants): Angel Studios resume and skills additions

--- assets/constants/skillConstants.js
+++ assets/constants/skillConstants.js
@@ -1,138 +1,125 @@
-import ArduinoIcon from "icons/ArduinoIcon";
-import Auth0Icon from "icons/Auth0Icon";
-import AwsIcon from "icons/AwsIcon";
-import BitbucketIcon from "icons/BitbucketIcon";
-import BootstrapIcon from "icons/BootstrapIcon";
-import ComposerIcon from "icons/ComposerIcon";
-import Css3Icon from "icons/Css3Icon";
-import DockerIcon from "icons/DockerIcon";
-import EclipseIcon from "icons/EclipseIcon";
-import FoundationIcon from "icons/FoundationIcon";
-import GitIcon from "icons/GitIcon";
-import GitHubIcon from "icons/GitHubIcon";
-import GraphQLIcon from "icons/GraphQLIcon";
-import HerokuIcon from "icons/HerokuIcon";
-import Html5Icon from "icons/Html5Icon";
-import JavaIcon from "icons/JavaIcon";
-import JavaScriptIcon from "icons/JavaScriptIcon";
-import JetBrainsIcon from "icons/JetBrainsIcon";
-import JenkinsIcon from "icons/JenkinsIcon";
-import KubernetesIcon from "icons/KubernetesIcon";
-import MaterialUIIcon from "icons/MaterialUIIcon";
-import MysqlIcon from "icons/MysqlIcon";
-import NodejsIcon from "icons/NodejsIcon";
-import NginxIcon from "icons/NginxIcon";
-import OracleIcon from "icons/OracleIcon";
-import PhpIcon from "icons/PhpIcon";
-import PostgresqlIcon from "icons/PostgresqlIcon";
-import PythonIcon from "icons/PythonIcon";
-import RabbitMQIcon from "icons/RabbitMQIcon";
-import RaspberryPiIcon from "icons/RaspberryPiIcon";
-import ReactIcon from "icons/ReactIcon";
-import RedisIcon from "icons/RedisIcon";
-import ReduxIcon from "icons/ReduxIcon";
-import SassIcon from "icons/SassIcon";
-import SpringIcon from "icons/SpringIcon";
-import StorybookIcon from "icons/StorybookIcon";
-import SwaggerIcon from "icons/SwaggerIcon";
-import SymfonyIcon from "icons/SymfonyIcon";
-import VisualStudioCodeIcon from "icons/VisualStudioCodeIcon";
-import WebpackIcon from "icons/WebpackIcon";
+import ArduinoIcon from 'icons/ArduinoIcon';
+import Auth0Icon from 'icons/Auth0Icon';
+import AwsIcon from 'icons/AwsIcon';
+import BitbucketIcon from 'icons/BitbucketIcon';
+import BootstrapIcon from 'icons/BootstrapIcon';
+import ComposerIcon from 'icons/ComposerIcon';
+import Css3Icon from 'icons/Css3Icon';
+import DockerIcon from 'icons/DockerIcon';
+import EclipseIcon from 'icons/EclipseIcon';
+import FoundationIcon from 'icons/FoundationIcon';
+import GitIcon from 'icons/GitIcon';
+import GitHubIcon from 'icons/GitHubIcon';
+import GraphQLIcon from 'icons/GraphQLIcon';
+import HerokuIcon from 'icons/HerokuIcon';
+import Html5Icon from 'icons/Html5Icon';
+import JavaIcon from 'icons/JavaIcon';
+import JavaScriptIcon from 'icons/JavaScriptIcon';
+import JetBrainsIcon from 'icons/JetBrainsIcon';
+import JenkinsIcon from 'icons/JenkinsIcon';
+import KubernetesIcon from 'icons/KubernetesIcon';
+import MaterialUIIcon from 'icons/MaterialUIIcon';
+import MysqlIcon from 'icons/MysqlIcon';
+import NodejsIcon from 'icons/NodejsIcon';
+import NginxIcon from 'icons/NginxIcon';
+import OracleIcon from 'icons/OracleIcon';
+import PhpIcon from 'icons/PhpIcon';
+import PostgresqlIcon from 'icons/PostgresqlIcon';
+import PythonIcon from 'icons/PythonIcon';
+import RabbitMQIcon from 'icons/RabbitMQIcon';
+import RaspberryPiIcon from 'icons/RaspberryPiIcon';
+import ReactIcon from 'icons/ReactIcon';
+import RedisIcon from 'icons/RedisIcon';
+import ReduxIcon from 'icons/ReduxIcon';
+import SassIcon from 'icons/SassIcon';
+import SpringIcon from 'icons/SpringIcon';
+import StorybookIcon from 'icons/StorybookIcon';
+import SwaggerIcon from 'icons/SwaggerIcon';
+import SymfonyIcon from 'icons/SymfonyIcon';
+import TailwindIcon from 'icons/TailwindIcon';
+import VercelIcon from 'icons/VercelIcon';
+import VisualStudioCodeIcon from 'icons/VisualStudioCodeIcon';
+import WebpackIcon from 'icons/WebpackIcon';
 
 export const skills = [
   {
-    title: "JavaScript",
+    title: 'JavaScript',
     icons: [JavaScriptIcon, ReactIcon, ReduxIcon],
-    body:
-      "JavaScript front-end development focusing on React.js with hooks, Redux.js, and Thunk.",
+    body: 'JavaScript front-end development focusing on React.js with hooks, Redux.js, and Thunk.',
   },
   {
-    title: "PHP",
+    title: 'PHP',
     icons: [PhpIcon, SymfonyIcon, ComposerIcon],
-    body:
-      "PHP web development utilizing the Symfony framework. 💕 Doctrine, Twig, & Api Platform!",
+    body: 'PHP web development utilizing the Symfony framework. 💕 Doctrine, Twig, & Api Platform!',
   },
   {
-    title: "HTML5 & CSS3",
+    title: 'HTML5 & CSS3',
     icons: [Html5Icon, Css3Icon, SassIcon],
-    body: "Web development 101 with some extra Sass flavoring.",
+    body: 'Web development 101 with some extra Sass flavoring.',
   },
   {
-    title: "Python",
+    title: 'Python',
     icons: [PythonIcon],
-    body:
-      "Scripting and RESTful API development with Python3, Pyramid, Sphinx, Alembic, and Django.",
+    body: 'Scripting and RESTful API development with Python3, Pyramid, Sphinx, Alembic, and Django.',
   },
   {
-    title: "Java",
+    title: 'Java',
     icons: [JavaIcon, SpringIcon],
-    body:
-      "J2EE Applications with Maven, modular design using OSGi, microservies with Spring and Hibernate.",
+    body: 'J2EE Applications with Maven, modular design using OSGi, microservies with Spring and Hibernate.',
   },
   {
-    title: "Microcontrollers",
+    title: 'Microcontrollers',
     icons: [ArduinoIcon, RaspberryPiIcon],
-    body:
-      "Designing fun projects for around the house automation and other gadgets using Arduinos and Raspberry Pis.",
+    body: 'Designing fun projects for around the house automation and other gadgets using Arduinos and Raspberry Pis.',
   },
   {
-    title: "Documentation",
+    title: 'Documentation',
     icons: [SwaggerIcon, StorybookIcon],
-    body:
-      "Generate Swagger documentation for API endpoints. Store reusable, extendable, and customizable React components in Storybook.",
+    body: 'Generate Swagger documentation for API endpoints. Store reusable, extendable, and customizable React components in Storybook.',
   },
   {
-    title: "DevOps",
+    title: 'DevOps',
     icons: [DockerIcon, KubernetesIcon, JenkinsIcon],
-    body:
-      "Deployment pipelines for Dockerized containers hosted on Kubernetes clusters and perform tests automation with Jenkins.",
+    body: 'Deployment pipelines for Dockerized containers hosted on Kubernetes clusters and perform tests automation with Jenkins.',
   },
   {
-    title: "Styling",
-    icons: [MaterialUIIcon, BootstrapIcon, FoundationIcon],
-    body:
-      "Work with the latest front-end web frameworks and libraries such as Material-UI, Bootstrap, and Foundation.",
+    title: 'Styling',
+    icons: [MaterialUIIcon, BootstrapIcon, TailwindIcon, FoundationIcon],
+    body: 'Work with the latest front-end web frameworks and libraries such as Tailwind, Material-UI, Bootstrap, and Foundation.',
   },
   {
-    title: "IAM",
+    title: 'IAM',
     icons: [Auth0Icon],
-    body:
-      "Sign in, authorize, and manage users with Auth0 standards and Okta. Provide Single Sign-On (SSO) for platforms.",
+    body: 'Sign in, authorize, and manage users with Auth0 standards and Okta. Provide Single Sign-On (SSO) for platforms.',
   },
   {
-    title: "Databases",
+    title: 'Databases',
     icons: [PostgresqlIcon, MysqlIcon, OracleIcon],
-    body:
-      "Management, administration, and architecture for OLTP and OLAP databases. Familiar with Oracle, PostgreSQL, and MySQL DBMS.",
+    body: 'Management, administration, and architecture for OLTP and OLAP databases. Familiar with Oracle, PostgreSQL, and MySQL DBMS.',
   },
   {
-    title: "Tools",
+    title: 'Tools',
     icons: [RabbitMQIcon, RedisIcon, NginxIcon],
-    body:
-      "RabbitMQ topics galore. In-memory data stores and caching with Redis. Nginx web server. Create reduncy and efficiency for your applications.",
+    body: 'RabbitMQ topics galore. In-memory data stores and caching with Redis. Nginx web server. Create reduncy and efficiency for your applications.',
   },
   {
-    title: "Version Control",
+    title: 'Version Control',
     icons: [GitIcon, GitHubIcon, BitbucketIcon],
-    body:
-      "Can you image a world before Git? I shudder to think about programming without GitHub or Bitbucket.",
+    body: 'Can you image a world before Git? I shudder to think about programming without GitHub or Bitbucket.',
   },
   {
-    title: "Misc",
+    title: 'Misc',
     icons: [GraphQLIcon, WebpackIcon, NodejsIcon],
-    body:
-      "The latest APIs with GraphQL. Bundle up applications with Webpack, Babel, Node.js, and Symfony Encore.",
+    body: 'The latest APIs with GraphQL. Bundle up applications with Webpack, Babel, Node.js, and Symfony Encore.',
   },
   {
-    title: "IDEs",
+    title: 'IDEs',
     icons: [VisualStudioCodeIcon, JetBrainsIcon, EclipseIcon],
-    body:
-      "A developer's best friend. Visual Studio Code 💯 because its free but JetBrains makes great products as well.",
+    body: "A developer's best friend. Visual Studio Code 💯 because its free but JetBrains makes great products as well.",
   },
   {
-    title: "Cloud Computing",
-    icons: [AwsIcon, HerokuIcon],
-    body:
-      "Utilize cloud infrastructure such as Heroku, and AWS (Elastic Beanstalk, RDS, S3, Route 53).",
+    title: 'Cloud Computing',
+    icons: [AwsIcon, HerokuIcon, VercelIcon],
+    body: 'Utilize cloud infrastructure such as Heroku, Vercel, and AWS (Elastic Beanstalk, RDS, S3, Route 53).',
   },
 ];
