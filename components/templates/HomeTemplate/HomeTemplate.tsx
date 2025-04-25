e4955a9ddcf2e91e381ca9bb8d41d4ba7f23c355
Cory Siebler <cory.siebler@protonmail.com>
2025-04-25 14:47:12 -0700

    feat: revising the TailwindCSS colors with different primary (#9)

--- components/templates/HomeTemplate/HomeTemplate.tsx
+++ components/templates/HomeTemplate/HomeTemplate.tsx
@@ -1,7 +1,21 @@
+import { Text } from '@/atoms/Text';
+
 export function HomeTemplate() {
   return (
-    <div>
-      <h1>Home Template</h1>
-    </div>
+    <main className="container bg-white p-4">
+      <div className="flex flex-col gap-1">
+        <Text variant="h1" weight="light">
+          Cory Siebler
+        </Text>
+        <Text
+          className="text-tint-darker"
+          component="h2"
+          variant="h4"
+          weight="light"
+        >
+          Senior Software Engineer
+        </Text>
+      </div>
+    </main>
   );
 }
