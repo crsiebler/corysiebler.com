8c1e82b1b7563ebcf46ecc26aae36e7825933a20
Cory Siebler <cory.siebler@protonmail.com>
2025-05-09 12:13:23 -0700

    feat: styling for medium screen size plus on Home template (#17)

--- components/templates/HomeTemplate/HomeTemplate.tsx
+++ components/templates/HomeTemplate/HomeTemplate.tsx
@@ -4,9 +4,11 @@ import { HomeSkillsSection } from './HomeSkillsSection';
 export function HomeTemplate() {
   return (
     <main className="bg-white py-4">
-      <HomeHeroSection />
-      <hr className="border-tint-light my-4 border-t" />
-      <HomeSkillsSection />
+      <div className="container mx-auto max-w-7xl">
+        <HomeHeroSection />
+        <hr className="border-tint-light my-4 border-t" />
+        <HomeSkillsSection />
+      </div>
     </main>
   );
 }
