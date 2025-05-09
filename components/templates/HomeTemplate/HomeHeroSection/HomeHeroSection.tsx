b32ce91d9a54e537f1c870298061a43fc5e9ba36
Cory Siebler <cory@oneorigin.us>
2025-05-09 11:38:05 -0700

    feat: styling for medium screen size plus on Home template

--- components/templates/HomeTemplate/HomeHeroSection/HomeHeroSection.tsx
+++ components/templates/HomeTemplate/HomeHeroSection/HomeHeroSection.tsx
@@ -6,65 +6,86 @@ import { Text } from '@/atoms/Text';
 
 export function HomeHeroSection() {
   return (
-    <section className="flex flex-col gap-4 px-4" id="hero">
-      <Text variant="h1" weight="light">
-        Cory Siebler
-      </Text>
-      <Text
-        className="text-tint-darkest"
-        component="h2"
-        variant="h4"
-        weight="light"
-      >
-        Senior Software Engineer
-      </Text>
-      <Text className="text-shade" component="p" variant="body1">
-        I am a full-stack developer specializing in web development with Next.js
-        and Python. Want to see more of my work? Check out my{' '}
-        <Link
-          className="text-blue hover:underline"
-          href="https://github.com/crsiebler"
-          target="_blank"
-          rel="noreferrer"
+    <section
+      className="mx-auto flex max-w-7xl flex-col gap-4 px-4 md:flex-row"
+      id="hero"
+    >
+      <div className="flex flex-col gap-4">
+        <Text variant="h1" weight="light">
+          Cory Siebler
+        </Text>
+        <Text
+          className="text-tint-darkest"
+          component="h2"
+          variant="h4"
+          weight="light"
         >
-          GitHub
-        </Link>{' '}
-        account.
-      </Text>
-      <div className="flex gap-2">
-        <LinkButton
-          className="h-full"
-          color="primary"
-          href="/resume"
-          variant="contained"
-        >
-          <span className="flex flex-row gap-2">
-            <Text
-              component="span"
-              className="my-auto uppercase"
-              variant="body2"
-            >
-              Resume
-            </Text>
-            <InfoIcon size={20} />
-          </span>
-        </LinkButton>
-        <LinkButton
-          className="h-full"
-          color="primary"
-          href="/portfolio"
-          variant="outlined"
-        >
-          <Text
-            component="span"
-            className="mx-auto flex flex-row uppercase"
-            variant="body2"
+          Senior Software Engineer
+        </Text>
+        <Text className="text-shade" component="p" variant="body1">
+          I am a full-stack developer specializing in web development with
+          Next.js and Python. Want to see more of my work? Check out my{' '}
+          <Link
+            className="text-blue hover:underline"
+            href="https://github.com/crsiebler"
+            target="_blank"
+            rel="noreferrer"
           >
-            Portfolio
-          </Text>
-        </LinkButton>
+            GitHub
+          </Link>{' '}
+          account.
+        </Text>
+        <div className="flex gap-2">
+          <LinkButton
+            className="h-full"
+            color="primary"
+            href="/resume"
+            variant="contained"
+          >
+            <span className="flex flex-row gap-2">
+              <Text
+                component="span"
+                className="my-auto hidden uppercase md:block"
+                variant="body2"
+              >
+                View Resume
+              </Text>
+              <Text
+                component="span"
+                className="my-auto uppercase md:hidden"
+                variant="body2"
+              >
+                Resume
+              </Text>
+              <InfoIcon size={20} />
+            </span>
+          </LinkButton>
+          <LinkButton
+            className="h-full"
+            color="primary"
+            href="/portfolio"
+            variant="outlined"
+          >
+            <span className="flex flex-row">
+              <Text
+                component="span"
+                className="hidden uppercase md:block"
+                variant="body2"
+              >
+                See Portfolio
+              </Text>
+              <Text
+                component="span"
+                className="uppercase md:hidden"
+                variant="body2"
+              >
+                Portfolio
+              </Text>
+            </span>
+          </LinkButton>
+        </div>
       </div>
-      <div className="flex w-full items-center justify-center">
+      <div className="flex w-full items-center justify-center md:w-fit">
         <HeadshotImage />
       </div>
     </section>
--- components/templates/HomeTemplate/HomeSkillsSection/HomeSkillsGrid.tsx
+++ components/templates/HomeTemplate/HomeSkillsSection/HomeSkillsGrid.tsx
@@ -4,7 +4,7 @@ import { SkillCard } from '@/molecules/SkillCard/SkillCard';
 
 export function HomeSkillsGrid() {
   return (
-    <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3">
+    <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
       {Object.entries(skills).map(([skillName, { description, icons }]) => (
         <SkillCard
           key={skillName}
