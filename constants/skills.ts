dad0b20fc454c58e0b687fbe8ef63af1a4548a77
Cory Siebler <cory.siebler@protonmail.com>
2025-06-13 13:38:32 -0700

    chore: adding Artificial Intelligence skills (#27)

--- constants/skills.ts
+++ constants/skills.ts
@@ -1,81 +1,86 @@
 export const skills = {
   JavaScript: {
     description:
-      'JavaScript front-end development focusing on 💕 Next.js and React.js with hooks.',
+      '✨ JavaScript front-end development focusing on 💕 Next.js and ⚛️ React.js with hooks.',
     icons: ['JavaScriptIcon', 'ReactIcon', 'NextJsIcon'],
   },
   'HTML5 & CSS3': {
-    description: 'Web development 101 with some extra Sass flavoring.',
+    description: '🌐 Web development 101 with some extra Sass flavoring 💅.',
     icons: ['Html5Icon', 'Css3Icon', 'SassIcon'],
   },
   Styling: {
     description:
-      'Work with the latest front-end web frameworks and libraries such as Material-UI, Chakra-UI, and ✌🏻 Tailwind CSS.',
+      '🎨 Work with the latest front-end web frameworks and libraries such as Material-UI, Chakra-UI, and ✌🏻 Tailwind CSS.',
     icons: ['TailwindIcon', 'ChakraUiIcon', 'MaterialUiIcon'],
   },
   Python: {
     description:
-      'Scripting and RESTful API development with Python3, Flask, Sphinx, Alembic, and Django.',
+      '🐍 Scripting and RESTful API development with Python3, Flask, Sphinx, Alembic, and Django.',
     icons: ['PythonIcon', 'DjangoIcon', 'FlaskIcon'],
   },
   Java: {
     description:
-      'J2EE Applications with Maven, modular design using OSGi, microservices with Spring and Hibernate.',
+      '☕ J2EE Applications with Maven, modular design using OSGi, microservices with Spring and Hibernate.',
     icons: ['JavaIcon', 'SpringIcon'],
   },
   PHP: {
     description:
-      'PHP web development utilizing the Symfony framework. Doctrine, Twig, & Api Platform!',
+      '🐘 PHP web development utilizing the Symfony framework. Doctrine, Twig, & Api Platform!',
     icons: ['PhpIcon', 'SymfonyIcon', 'ComposerIcon'],
+  },
+  'Artificial Intelligence': {
+    description:
+      '🤖 Experience using AI tools such as GitHub Copilot, ChatGPT, DeepSeek, Midjourney, and more to enhance productivity and creativity.',
+    icons: ['CopilotIcon', 'ChatGptIcon', 'DeepSeekIcon', 'MidjourneyIcon'],
   },
   Documentation: {
     description:
-      'Generate Swagger documentation for API endpoints. Store reusable, extendable, and customizable React components in Storybook.',
+      '📝 Generate Swagger documentation for API endpoints. Store reusable, extendable, and customizable React components in Storybook.',
     icons: ['SwaggerIcon', 'StorybookIcon'],
   },
   DevOps: {
     description:
-      'Deployment pipelines for Dockerized containers hosted on Kubernetes clusters and perform tests automation with Jenkins.',
+      '🚀 Deployment pipelines for Dockerized containers hosted on Kubernetes clusters and perform tests automation with Jenkins.',
     icons: ['DockerIcon', 'KubernetesIcon', 'JenkinsIcon'],
   },
   Microcontrollers: {
     description:
-      'Designing fun projects for around the house automation and other gadgets using Arduino and Raspberry Pis.',
+      '🤖 Designing fun projects for around the house automation and other gadgets using Arduino and Raspberry Pis.',
     icons: ['ArduinoIcon', 'RaspberryPiIcon'],
   },
   IAM: {
     description:
-      'Sign in, authorize, and manage users with Auth0 standards. Provide Single Sign-On (SSO) for platforms.',
+      '🔐 Sign in, authorize, and manage users with Auth0 standards. Provide Single Sign-On (SSO) for platforms.',
     icons: ['Auth0Icon'],
   },
   Databases: {
     description:
-      'Management, administration, and architecture for OLTP and OLAP databases. Familiar with Oracle, PostgreSQL, and MySQL DBMS.',
+      '🗄️ Management, administration, and architecture for OLTP and OLAP databases. Familiar with Oracle, PostgreSQL, and MySQL DBMS.',
     icons: ['PostgreSqlIcon', 'MySqlIcon', 'OracleIcon'],
   },
   Tools: {
     description:
-      'RabbitMQ topics galore. In-memory data stores and caching with Redis. Nginx web server. Create redundancy and efficiency for your applications.',
+      '🛠️ RabbitMQ topics galore. In-memory data stores and caching with Redis. Nginx web server. Create redundancy and efficiency for your applications.',
     icons: ['RabbitMqIcon', 'RedisIcon', 'NginxIcon'],
   },
   'Version Control': {
     description:
-      'Can you imagine a world before Git? I shudder to think about programming without GitHub or Bitbucket.',
+      '🔄 Can you imagine a world before Git? I shudder to think about programming without GitHub or Bitbucket.',
     icons: ['GitIcon', 'GitHubIcon', 'BitbucketIcon'],
   },
   IDEs: {
     description:
-      "A developer's best friend. Visual Studio Code 💯 because it's free but JetBrains makes great products as well.",
+      "💻 A developer's best friend. Visual Studio Code 💯 because it's free but JetBrains makes great products as well.",
     icons: ['VisualStudioCodeIcon', 'JetBrainsIcon'],
   },
   'Cloud Computing': {
     description:
-      'Utilize cloud infrastructure such as Heroku, Vercel, and AWS (Lambda, EC2, RDS, S3, Neptune).',
+      '☁️ Utilize cloud infrastructure such as Heroku, Vercel, and AWS (Lambda, EC2, RDS, S3, Neptune).',
     icons: ['AwsIcon', 'HerokuIcon', 'VercelIcon'],
   },
   Misc: {
     description:
-      'The latest APIs with GraphQL. Bundle up applications with Webpack, Babel, and Node.js.',
+      '🧩 The latest APIs with GraphQL. Bundle up applications with Webpack, Babel, and Node.js.',
     icons: ['GraphqlIcon', 'WebpackIcon', 'NodeJsIcon', 'BabelIcon'],
   },
 };
