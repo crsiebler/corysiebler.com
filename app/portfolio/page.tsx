48c4dc4e079d16e427fd6c60e0cb843cbaa9953f
Cory Siebler <cory.siebler@protonmail.com>
2026-08-14 18:36:06 -0700

    chore: revising job title Senior to Principal (#38)

--- app/portfolio/page.tsx
+++ app/portfolio/page.tsx
@@ -4,7 +4,7 @@ import { PortfolioTemplate } from '@/templates/PortfolioTemplate';
 export const metadata = getMetadata({
   title: 'Portfolio',
   description:
-    'Portfolio of Cory Siebler, Senior Software Engineer. Projects, case studies, and open source work in web development, Next.js, and Python.',
+    'Portfolio of Cory Siebler, Principal Software Engineer. Projects, case studies, and open source work in web development, Next.js, and Python.',
   path: '/portfolio',
 });
 
